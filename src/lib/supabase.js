b165af8a8b174143cac4423964aa94c2c6cdf57c
Douglas <douglas.s.sodre@gmail.com>
2025-12-31 14:22:56 -0300

    feat: Add Supabase client configuration and update `.gitignore` to ignore `.env` files.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://sfbmelnwdslnyyyzxlzb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmYm1lbG53ZHNsbnl5eXp4bHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NTU1NDIsImV4cCI6MjA4MjMzMTU0Mn0.FasE7Iahi3RgdR7xm5eCUvzJAeM8s1ibpu-1gDlW76Y'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
